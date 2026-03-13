ee2b302e57f33cb7c2c519fcf167a3e02ff047af
Aaron Griffiths <aa.griff9@gmail.com>
2026-03-12 23:05:12 -0700

    update build scripts

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,5 +1,7 @@
 import * as Sentry from "@sentry/nextjs";
 
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
+
 const dsn = process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 if (dsn) {
